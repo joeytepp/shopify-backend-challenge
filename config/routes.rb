84597362846c1c9a518cbfa0ab1b7cebde0b5332
Joey Tepperman <jmichaelt98@gmail.com>
2019-01-16 18:45:06 -0500

    :white_check_mark: Add default route to /playground

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphqlPlayground::Rails::Engine, at: "/playground", graphql_path: "/graphql"
   end
+
+  get "*path", to: redirect("/playground")
 end
